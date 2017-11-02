4c884871d7e9e5503fb5ec1e35637ae093731602
Gabriel de Oliveira Barbosa <manobi.oliveira@gmail.com>
2017-11-02 21:33:31 -0200

    Add context and fns

---
+++ context/main.js
@@ -0,0 +1,21 @@
+/*
+* ## This
+*/
+var anakin   = {};
+let quiGon   = {};
+const obiWan = {};
+jocastaNu    = {};
+
+/*
+* ### Global
+*/
+console.log(this);
+
+var yoda = {
+  height: 0.7,
+  color: 'green',
+  age: 896
+};
+
+console.log(yoda);
+console.log(this.yoda);
--- std/function.js
+++ std/function.js
@@ -23,7 +23,7 @@ let calculate = function(x, y) {
 * ## Function constructor
 */
 let adder = new Function('a', 'b', 'return a + b');
-console.log( adder(1, 1) );
+//console.log( adder(1, 1) );
 
 /*
 * ## Properties
@@ -32,12 +32,12 @@ console.log( adder(1, 1) );
 /*
 * name
 */
-console.log(calculate.name);
+//console.log(calculate.name);
 
 /*
 * arguments
 */
-console.log('calculate arguments', calculate.length);
+//console.log('calculate arguments', calculate.length);
 
 /*
 * ## Methods
@@ -59,7 +59,7 @@ let person = {
 */
 for (key in person) {
   if (typeof person[key] === 'function') {
-    person[key].call();
+    //person[key].call();
   };
 }
 
@@ -68,6 +68,51 @@ for (key in person) {
 */
 for (key in person) {
   if (typeof person[key] === 'function') {
-    person[key].apply();
+    //person[key].apply();
   };
 }
+
+/*
+* ### IIFE  Imediately Invoked Function Expression
+*/
+(function(){
+  //console.log('Executou');
+})();
+
+/*
+* ### Callback
+*/
+function pGify(text, callback) {
+  let str = text.split(' ');
+  show(str.join(' p'));
+}
+
+
+function show(msg){
+  //https://coderwall.com/p/yphywg/printing-colorful-text-in-terminal-when-run-node-js-script
+  console.log('\x1b[33m%s\x1b[0m: ', 'RESULT');  //yellow
+  console.log('\x1b[36m%s\x1b[0m', '"' + msg + '"');  //cyan
+  console.log('\x1b[31m%s\x1b[0m', 'A menssagem possui ' + msg.length + ' caracteres');  //cyan
+}
+//show('Menssagem de teste');
+
+//pGify('It is a period of civil war...', show);
+
+/*
+* ### Closure
+*/
+function pGify2 (text){
+  let str = text.split(' ');
+  let pgfied = str.join(' p');
+
+  function show(){
+    console.log('\x1b[33m%s\x1b[0m: ', 'RESULT');  //yellow
+    console.log('\x1b[36m%s\x1b[0m', '"' + pgfied + '"');  //cyan
+    console.log('\x1b[31m%s\x1b[0m', 'A menssagem possui ' + pgfied.length + ' caracteres');  //cyan
+  }
+
+  return show;
+}
+
+//let pgfiedText = pGify2('It is a period of civil war...');
+//pgfiedText();
